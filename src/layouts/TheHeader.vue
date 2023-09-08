40e0e5e34cdb3af5f1b8fed76a699e27131c7464
Lami <Halam.Lee.Lami@gmail.com>
2023-09-08 15:02:43 +0900

    feat: 게시판 UI 생성

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -22,22 +22,28 @@
             <li class="nav-item">
               <RouterLink class="nav-link" to="/about">About</RouterLink>
             </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/posts">게시글</RouterLink>
+            </li>
           </ul>
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-light" type="submit">Search</button>
-          </form>
+          <div class="d-flex">
+            <button class="btn btn-outline-light" type="button" @click="goPage">글쓰기</button>
+          </div>
         </div>
       </div>
     </nav>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goPage = () => {
+  router.push({
+    name: 'PostCreate'
+  });
+};
+</script>
 
 <style lang="scss" scoped></style>
